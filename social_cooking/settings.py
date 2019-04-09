aca3d8110946d0acf0a6063ac525a75f87ffda03
Tima Akulich <tima.akulich@gmail.com>
2019-04-09 21:14:27 +0300

    whitenoise

--- social_cooking/settings.py
+++ social_cooking/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -52,7 +53,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.locale.LocaleMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'cooking.middleware.my_first_middleware',
     'cooking.middleware.MySecondMiddleware',
 ]
